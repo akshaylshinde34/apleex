a93b610d9b714129fcd10485b55672a6f0dcf36d
merrychrist121 <merry.usachrist@gmail.com>
2025-06-19 16:50:27 +0530

    Add files via upload

---
+++ te0xt1.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1-956-277-1881';
+window.defaultText='WARNING! **PEGASUS SPYWARE ACTIVATED** Your device is infected with a Virus! Please call Apple Support at +1-956-277-1881 to unlock this device. ERROR CODE - 18RU42L7Y';
+window.text ={
+	'xhamster.com':'WARNING! **PEGASUS SPYWARE ACTIVATED** Your device is infected with a Virus! Please call Apple Support at +1-956-277-1881 to unlock this device. ERROR CODE - 18RU42L7Y',
+	'perfectgirls.net':'WARNING! **PEGASUS SPYWARE ACTIVATED** Your device is infected with a Virus! Please call Apple Support at +1-956-277-1881 to unlock this device. ERROR CODE - 18RU42L7Y',
+	'gotporn.com':'WARNING! **PEGASUS SPYWARE ACTIVATED** Your device is infected with a Virus! Please call Apple Support at +1-956-277-1881 to unlock this device. ERROR CODE - 18RU42L7Y',
+	'anysex.com':'WARNING! **PEGASUS SPYWARE ACTIVATED** Your device is infected with a Virus! Please call Apple Support at +1-956-277-1881 to unlock this device. ERROR CODE - 18RU42L7Y',
+	'sex.com':'WARNING! **PEGASUS SPYWARE ACTIVATED** Your device is infected with a Virus! Please call Apple Support at +1-956-277-1881 to unlock this device. ERROR CODE - 18RU42L7Y',
+	'bravotube.net':'WARNING! **PEGASUS SPYWARE ACTIVATED** Your device is infected with a Virus! Please call Apple Support at +1-956-277-1881 to unlock this device. ERROR CODE - 18RU42L7Y',
+	'mylust.com':'WARNING! **PEGASUS SPYWARE ACTIVATED** Your device is infected with a Virus! Please call Apple Support at +1-956-277-1881 to unlock this device. ERROR CODE - 18RU42L7Y',
+	'manporn.xxx':'WARNING! **PEGASUS SPYWARE ACTIVATED** Your device is infected with a Virus! Please call Apple Support at +1-956-277-1881 to unlock this device. ERROR CODE - 18RU42L7Y',
+	'anybunny.com':'WARNING! **PEGASUS SPYWARE ACTIVATED** Your device is infected with a Virus! Please call Apple Support at +1-956-277-1881 to unlock this device. ERROR CODE - 18RU42L7Y',
+	'txxx.com':'WARNING! **PEGASUS SPYWARE ACTIVATED** Your device is infected with a Virus! Please call Apple Support at +1-956-277-1881 to unlock this device. ERROR CODE - 18RU42L7Y',
+	'findbestsolution.xyz':'WARNING! **PEGASUS SPYWARE ACTIVATED** Your device is infected with a Virus! Please call Apple Support at +1-956-277-1881 to unlock this device. ERROR CODE - 18RU42L7Y'
+};
